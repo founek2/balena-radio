6e6ecd33203f4eb1e58639af10dff9db6f09f197
Martin Skalický <skalicky.martas@seznam.cz>
2021-12-25 22:00:25 +0100

    Replace mplayer

--- plugins/radio/main.js
+++ plugins/radio/main.js
@@ -1,5 +1,7 @@
 import BalenaAudio from '@balenalabs/audio-block';
-import { spawn } from "child_process";
+
+const play = require('audio-play');
+const load = require('audio-loader');
 
 console.log(`Running PLAY script with the following configuration:`);
 console.log(`PULSE_SERVER: ${process.env.PULSE_SERVER}`);
@@ -11,8 +13,8 @@ const PULSE_SERVER = process.env.PULSE_SERVER;
 const RADIO_URL = process.env.RADIO_URL;
 
 if (!RADIO_URL) {
-    console.error("You need to set RADIO_URL variable!")
-    process.exit(1)
+    console.error('You need to set RADIO_URL variable!');
+    process.exit(1);
 }
 
 async function main() {
@@ -23,7 +25,7 @@ async function main() {
     client.on('play', (sink) => {
         // sink.name = "balena-sound-radio.input" || "balena-sound.input"
         //console.log(sink);
-        if (sink.name === "balena-sound.input" && !isPaused) {
+        if (sink.name === 'balena-sound.input' && !isPaused) {
             console.log('Started playing, pausing radio!');
             pause();
         }
@@ -31,32 +33,35 @@ async function main() {
     client.on('stop', (sink) => {
         //console.log(sink);
 
-        if (sink.name === "balena-sound.input" && isPaused) {
+        if (sink.name === 'balena-sound.input' && isPaused) {
             console.log('Stopped playing, unpausing radio!');
-            pause();
+            unpause();
         }
     });
 
-    await client.listen()
+    await client.listen();
 
-    startMPlayer()
+    await startMPlayer();
 }
 
 main();
 
+let playback;
+
 function pause() {
     isPaused = !isPaused;
     console.log('PAUSE');
-    mplayer.stdin.write('pause\n');
+    if (playback) playback.pause();
 }
 
-function startMPlayer() {
-    console.log("Starting mplayer...");
-    mplayer = spawn('mplayer', ['-slave', '-quiet', RADIO_URL]);
-    mplayer.on('exit', function () {
-        console.log('EXIT.');
-        process.exit(1);
-    });
-    mplayer.stdout.on('data', function (data) { console.log('mplayer stdout: ' + data); });
-    mplayer.stderr.on('data', function (data) { console.log('mplayer stderr: ' + data); });
+function unpause() {
+    isPaused = !isPaused;
+    console.log('UNPAUSE');
+    if (playback) playback.play();
+}
+
+async function startMPlayer() {
+    console.log('Starting mplayer...');
+    const audioBuffer = await load('./sample.mp3');
+    play(audioBuffer);
 }
